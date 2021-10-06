687992533c39651f677e386e363cf356202b5303
Andre Lemes <dhl.andre.lemes@gmail.com>
2021-10-06 17:27:14 -0300

    Adicionar arquivos de projeto.

---
+++ ASP core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
